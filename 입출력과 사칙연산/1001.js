8146f5b97bd4783cc30c923b6f887bf044b04a04
ddo0ii <soyeong5857@gmail.com>
2022-10-17 15:19:35 +0900

    사칙연산

---
+++ 입출력과 사칙연산/1001.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+console.log(a - b);
---
+++ 입출력과 사칙연산/10869.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
