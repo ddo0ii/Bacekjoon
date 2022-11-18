c9c84e8ea36cf173bb1cb71a13435b981003a751
ddo0ii <soyeong5857@gmail.com>
2022-11-18 16:07:35 +0900

    숫자의 합

--- 문자열/11720.js
+++ 문자열/11720.js
@@ -5,6 +5,6 @@ const input1 = Number(input[0]);
 const input2 = input[1].split("");
 let sum = 0;
 for (let i = 0; i < input1; i++) {
-  sum = Number(input2[i]);
+  sum += Number(input2[i]);
 }
 console.log(sum);
